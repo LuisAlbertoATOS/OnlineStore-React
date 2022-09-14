9c15f7a189cab6ddea9cfa50f8ab1ced5899018b
Luis Perez <luis-alberto.perez@atos.net>
2022-09-14 09:31:22 -0500

    tailwind

---
+++ online-strore/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className='h-screen bg-green-400'>
+        <h1 className='h-full text-4xl font-bold'>Hello</h1>
+      </div>
+    </div>
+  );
+}
+
+export default App;
