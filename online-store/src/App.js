bdb9857bc91ead5d55829dac83d71d2db5fc919f
AlanGarciaAtos <alan.garcia@atos.net>
2022-09-15 15:39:00 -0500

    Add missing routes

--- online-store/src/App.js
+++ online-store/src/App.js
@@ -4,9 +4,17 @@ import { Route, Routes, Link } from 'react-router-dom';
 import Home from './components/Home/Home';
 import SignInForm from './forms/SignInForm';
 import ProductForm from './forms/ProductForm';
+import ShoppingCart from './components/ShoppingCart/ShoppingCart';
+
+// Components that aren't ready
+import AdminDashboard from './components/Admin/AdminDashboard';
+import Products from './components/Admin/Products';
+import Sales from './components/Admin/Sales';
+import ProductDetails from './components/Home/ProductDetails';
+import ProductList from './components/Home/ProductList';
+import UserInfoForm from './forms/UserInfoForm';
 
 import './App.css';
-import ShoppingCart from './components/ShoppingCart/ShoppingCart';
 
 function App() {
   return (
@@ -29,8 +37,21 @@ function App() {
       <Routes>
         <Route path="/*" element={<Home />} />
         <Route path="/sign-in" element={<SignInForm />} />
-        <Route path="/shopping-cart" element={<ShoppingCart />} />
+        <Route path="/shopping-cart/*" element={<ShoppingCart />} />
         <Route path="/product-form" element={<ProductForm action={'New'} />} />
+
+        {/* Routes below this comment doesn't work right now */}
+        {/* Admin */}
+        <Route path="/admin-dashboard" element={<AdminDashboard />} />
+        <Route path="/products" element={<Products />} />
+        <Route path="/sales" element={<Sales />} />
+
+        {/* Home */}
+        <Route path="/product-detail" element={<ProductDetails />} />
+        <Route path="/product-list" element={<ProductList />} />
+
+        {/* Forms */}
+        <Route path="/user-form" element={<UserInfoForm />} />
       </Routes>
 
       <h1 className="debug bg-orange-500">
--- online-store/src/components/ShoppingCart/SuccessfullPurchase.jsx
+++ online-store/src/components/ShoppingCart/SuccessfullPurchase.jsx
@@ -1,9 +1,11 @@
-import React from 'react'
+import React from 'react';
 
 const SuccessfullPurchase = () => {
   return (
-    <div>SuccessfullPurchase</div>
-  )
-}
+    <div className="italic font-semibold text-center text-2xl text-slate-100 bg-green-600 m-5">
+      SuccessfullPurchase
+    </div>
+  );
+};
 
-export default SuccessfullPurchase
+export default SuccessfullPurchase;
