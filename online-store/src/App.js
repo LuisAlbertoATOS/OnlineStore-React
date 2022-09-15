ff0ead4681074249b80a39ff1471e465ce34c2f9
Luis Perez <luis-alberto.perez@atos.net>
2022-09-15 15:00:19 -0500

    relative routing added

--- online-store/src/App.js
+++ online-store/src/App.js
@@ -1,26 +1,35 @@
 import React from 'react';
-import { Route, Routes } from 'react-router-dom';
+import { Route, Routes, Link } from 'react-router-dom';
 
 import Home from './components/Home/Home';
 import SignInForm from './forms/SignInForm';
 import ProductForm from './forms/ProductForm';
 
 import './App.css';
+import ShoppingCart from './components/ShoppingCart/ShoppingCart';
 
 //! Tailwind styles doesn't get recognized
 
 function App() {
   return (
     <React.Fragment>
+
+      <p><Link to='/'>Logo</Link></p>
+      <p><Link to='/sign-in'>Sign in</Link></p>
+      <p><Link to='/shopping-cart'>Shopping cart</Link></p>
+
+      {/* <p><Link to='/product-form'>New product</Link></p> */}
+
       <Routes>
         {/* All the path names are temporary */}
         <Route path="/" element={<Home />} />
         {/* Better naming convention for signIn || sign-in || signin  */}
         <Route path="/sign-in" element={<SignInForm />} />
-        <Route path="/product-form" element={<ProductForm />} />
+        <Route path="/shopping-cart" element={<ShoppingCart />} />
+        <Route path="/product-form" element={<ProductForm action={'New'} />} />
       </Routes>
 
-      <h1 className="debug">
+      <h1 className="debug bg-orange-500">
         This is a test, it will appear in every component{' '}
       </h1>
 
--- online-store/src/components/ShoppingCart/ShoppingCart.jsx
+++ online-store/src/components/ShoppingCart/ShoppingCart.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import React from "react";
+import { Route, Routes, Link } from "react-router-dom";
+import SuccessfullPurchase from "./SuccessfullPurchase";
 
 const ShoppingCart = () => {
   return (
-    <div>ShoppingCart</div>
-  )
-}
+    <div>
+      ShoppingCart
+      <p>
+        <Link to="successfull-purchase">Buy</Link>
+      </p>
+      <Routes>
+        <Route path="successfull-purchase" element={<SuccessfullPurchase />} />
+      </Routes>
+    </div>
+  );
+};
 
-export default ShoppingCart
+export default ShoppingCart;
