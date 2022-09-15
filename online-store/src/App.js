3758bac7cce63dd6f220d29c37370b32dc054dde
AlanSeyfir <alan.garcia.nunez@gmail.com>
2022-09-15 14:28:00 -0500

    Add routes, but are manual

--- online-store/src/App.js
+++ online-store/src/App.js
@@ -1,11 +1,34 @@
-import './App.css';
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+
+import Home from './components/Home/Home';
+import SignInForm from './forms/SignInForm';
 import ProductForm from './forms/ProductForm';
+
+import './App.css';
+
+//! Tailwind styles doesn't get recognized
 
 function App() {
   return (
-    <div className="App">
-      <ProductForm action={'New'}/>
-    </div>
+    <React.Fragment>
+      <Routes>
+        {/* All the path names are temporary */}
+        <Route path="/" element={<Home />} />
+        {/* Better naming convention for signIn || sign-in || signin  */}
+        <Route path="/sign-in" element={<SignInForm />} />
+        <Route path="/product-form" element={<ProductForm />} />
+      </Routes>
+
+      <h1 className="debug">
+        This is a test, it will appear in every component{' '}
+      </h1>
+
+      {/* What is the meaning of action={'New'} */}
+      {/* <div className="App">
+        <ProductForm action={'New'} />
+      </div> */}
+    </React.Fragment>
   );
 }
 
