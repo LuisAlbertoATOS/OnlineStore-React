5569649d04e4c075c689941483bcd67e526ae1af
Luis Perez <luis-alberto.perez@atos.net>
2022-09-19 10:03:11 -0500

    user info form template

--- online-store/src/App.js
+++ online-store/src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Route, Routes, Link } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
 
 import Home from "./components/Home/Home";
@@ -40,6 +40,7 @@ function App() {
         <Route path="/user-form" element={<UserInfoForm />} />
       </Routes>
     </section>
+
   );
 }
 
