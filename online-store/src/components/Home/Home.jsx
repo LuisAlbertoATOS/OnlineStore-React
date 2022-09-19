b58e948b16683448bbf3275730b59c2bc302a331
AlanGarciaAtos <alan.garcia@atos.net>
2022-09-19 12:32:55 -0500

    Add category card component, styles and image

--- online-store/src/components/Home/Home.jsx
+++ online-store/src/components/Home/Home.jsx
@@ -1,5 +1,6 @@
-import React from "react";
-import Navbar from "../Navbar";
+import React from 'react';
+import Navbar from '../Navbar';
+import CategoryCard from './CategoryCard';
 
 const Home = () => {
   return (
@@ -8,6 +9,8 @@ const Home = () => {
       <div className="italic font-semibold text-center text-2xl text-slate-100 bg-blue-600 m-5">
         Home
       </div>
+
+      <CategoryCard />
     </div>
   );
 };
