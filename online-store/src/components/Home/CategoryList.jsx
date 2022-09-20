0de5014be11b07b8fe41883c08b705c59d873995
AlanGarciaAtos <alan.garcia@atos.net>
2022-09-20 10:43:31 -0500

    Add multiple categories

--- online-store/src/components/Home/CategoryList.jsx
+++ online-store/src/components/Home/CategoryList.jsx
@@ -1,7 +1,54 @@
 import React from 'react';
+import CategoryCard from './CategoryCard';
 
 const CategoryList = () => {
-  return <div>CategoryList</div>;
+  const dummyCategories = [
+    {
+      productId: '12345',
+      name: 'Videogames',
+      description:
+        'Products related to videogames. Consoles,controllers, videogames, etc.',
+      category: 'Videogames',
+      image: 'https://i.imgur.com/cNVDCxs.png',
+      deleted: false,
+    },
+    {
+      productId: '12345',
+      name: 'Videogames',
+      description:
+        'Products related to videogames. Consoles,controllers, videogames, etc.',
+      category: 'Videogames',
+      image: 'https://i.imgur.com/cNVDCxs.png',
+      deleted: false,
+    },
+    {
+      productId: '12345',
+      name: 'Videogames',
+      description:
+        'Products related to videogames. Consoles,controllers, videogames, etc.',
+      category: 'Videogames',
+      image: 'https://i.imgur.com/cNVDCxs.png',
+      deleted: false,
+    },
+    {
+      productId: '12345',
+      name: 'Videogames',
+      description:
+        'Products related to videogames. Consoles,controllers, videogames, etc.',
+      category: 'Videogames',
+      image: 'https://i.imgur.com/cNVDCxs.png',
+      deleted: false,
+    },
+  ];
+
+  console.log(dummyCategories);
+  return (
+    <section className="flex flex-row flex-wrap justify-center gap-3">
+      {dummyCategories.map((category) => {
+        return <CategoryCard product={category} />;
+      })}
+    </section>
+  );
 };
 
 export default CategoryList;
--- online-store/src/components/Home/Home.jsx
+++ online-store/src/components/Home/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Navbar from '../Navbar';
 import CategoryCard from './CategoryCard';
 import { Link } from 'react-router-dom';
+import CategoryList from './CategoryList';
 
 const Home = () => {
   return (
@@ -20,7 +21,8 @@ const Home = () => {
         </button>
       </Link>
 
-      <CategoryCard />
+      {/* <CategoryCard /> */}
+      <CategoryList />
     </div>
   );
 };
