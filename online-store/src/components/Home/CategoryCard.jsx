2a718f2e4c2ed7d0ec493e6fb5b15dc7f907ed5c
Luis Perez <luis-alberto.perez@atos.net>
2022-09-21 15:27:03 -0500

    removed dummy data and ui fixes

--- online-store/src/components/Home/CategoryCard.jsx
+++ online-store/src/components/Home/CategoryCard.jsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import { Link, useParams } from 'react-router-dom';
+import React from "react";
+import { Link, useParams } from "react-router-dom";
 
 const CategoryCard = (props) => {
   const { id } = useParams();
@@ -8,16 +8,16 @@ const CategoryCard = (props) => {
   //? Price and stock don't need them right?
 
   return (
-    <section>
-      <div className="flex justify-center">
+    <section className="h-[25rem] my-4 mx-1">
+      <div className="h-full flex justify-center">
         {id}
         {/* Maybe the link change it's position */}
         <Link to={`category/${props.category.category}`}>
-          <div className="rounded-lg shadow-lg bg-white max-w-xs border-dashed border-2 border-sky-500">
+          <div className="h-full relative rounded-lg shadow-lg bg-white max-w-xs border-dashed border-2 border-sky-500">
             <div>
               <img
-                style={{ width: '100%' }}
-                className="rounded-t-lg px-5 py-5"
+                style={{ width: "100%" }}
+                className="rounded-t-lg px-5 pt-5"
                 src={props.category.image}
                 alt={props.category.description}
               />
@@ -31,12 +31,14 @@ const CategoryCard = (props) => {
                 {props.category.description}
               </p>
 
-              <button
-                type="button"
-                className="inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out disabled:bg-blue-400"
-              >
-                See {props.category.category} Category
-              </button>
+              <section className="absolute bottom-0 mb-6 w-5/6">
+                <button
+                  type="button"
+                  className="inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out disabled:bg-blue-400"
+                >
+                  See {props.category.category} Category
+                </button>
+              </section>
             </div>
           </div>
         </Link>
--- online-store/src/services/category.services.js
+++ online-store/src/services/category.services.js
@@ -12,27 +12,19 @@ import { array } from "zod";
 const categoryCollectionRef = collection(db, "categories");
 export class CategoryDataService {
 
-  getAllCategories = async () => {
-    const querySnapshot = await getDocs(categoryCollectionRef);
-
-    // console.log(querySnapshot);
-    // querySnapshot.forEach((doc) => {
-    //   // doc.data() is never undefined for query doc snapshots
-    //   console.log(doc.id, " => ", doc.data());
-    // });
-    
-    return getDocs(querySnapshot);
+  getAllCategories = () => {
+    return getDocs(categoryCollectionRef);
   };
 
-  getAllCategoryNames = async () => {
-    const querySnapshot = await getDocs(categoryCollectionRef);
+  // getAllCategoryNames = async () => {
+  //   const querySnapshot = await getDocs(categoryCollectionRef);
 
-    let categories = [];
-    querySnapshot.forEach((doc) => {
-      categories.push({ value: doc.data().category, text: doc.data().name });
-    });
-    console.log(categories);
+  //   let categories = [];
+  //   querySnapshot.forEach((doc) => {
+  //     categories.push({ value: doc.data().category, text: doc.data().name });
+  //   });
+  //   console.log(categories);
     
-    return categories;
-  };
+  //   return categories;
+  // };
 }
