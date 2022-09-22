1788b5f732cb0cb96be838defa7b8e2b0b1c8366
AlanGarciaAtos <alan.garcia@atos.net>
2022-09-22 14:40:34 -0500

    Remove unused code

--- online-store/src/components/Home/ProductDetails.jsx
+++ online-store/src/components/Home/ProductDetails.jsx
@@ -1,11 +1,5 @@
 import React from 'react';
-import { ProductDataService } from '../../services/product.services';
 import Navbar from '../Navbar';
-
-const helperProductService = new ProductDataService();
-
-// let test = helperProductService.getAllProducts();
-// console.log(test);
 
 const ProductDetails = (props) => {
   return (
@@ -26,14 +20,7 @@ const ProductDetails = (props) => {
               <h1 class="text-gray-900 text-3xl title-font font-medium mb-1">
                 Nintendo Switch
               </h1>
-              <p class="leading-relaxed">
-                Fam locavore kickstarter distillery. Mixtape chillwave tumeric
-                sriracha taximy chia microdosing tilde DIY. XOXO fam indxgo
-                juiceramps cornhole raw denim forage brooklyn. Everyday carry +1
-                seitan poutine tumeric. Gastropub blue bottle austin listicle
-                pour-over, neutra jean shorts keytar banjo tattooed umami
-                cardigan.
-              </p>
+              <p class="leading-relaxed">32 GB. Red and blue</p>
               <p class=" font-bold text-blue-500 mt-3 ">Stock: 60</p>
               <div class="flex items-center pb-5 border-b-2 border-gray-200 mb-5"></div>
               <div class="flex">
@@ -44,60 +31,6 @@ const ProductDetails = (props) => {
                   Add to cart
                 </button>
               </div>
-            </div>
-          </div>
-        </div>
-      </section>
-
-      {/* Temporary disable */}
-      <section className=" hidden h-[32rem] my-4 mx-4">
-        <div className="h-full flex justify-center">
-          {/* <Link to={`product-detail/${props.product.productId}`}> */}
-          <div className="h-full relative rounded-lg shadow-lg bg-white max-w-xs">
-            <div href="#!">
-              {/* <img
-                className="rounded-t-lg px-5 py-5"
-                src={props.product.image}
-                alt={props.product.name}
-              /> */}
-            </div>
-            <div className="p-6">
-              <h5 className="text-gray-900 text-xl font-medium mb-2">
-                {/* {props.product.name} */}
-              </h5>
-              {/* {props.product.description.length < 65 && (
-                  <p className="text-gray-700 text-base mb-4 text-left">
-                    {props.product.description}
-                  </p>
-                )} */}
-              <section className="absolute bottom-0 my-4 w-5/6">
-                {/* {props.product.stock === 0 && (
-                    <div>
-                      <p className="text-base mb-4 text-left text-red-700 font-bold">
-                        Not available
-                      </p>{" "}
-                      <button
-                        type="button"
-                        className="w-full inline-block px-6 py-2.5 bg-blue-200 text-white font-medium text-xs leading-tight uppercase rounded shadow-md"
-                      >
-                        Add to shopping cart
-                      </button>
-                    </div>
-                  )} */}
-                {/* {props.product.stock !== 0 && (
-                    <div className="">
-                      <p className="text-base mb-4 text-left text-green-700 font-bold">
-                        Price: ${props.product.price}
-                      </p>
-                      <button
-                        type="button"
-                        className="w-full inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out disabled:bg-blue-400"
-                      >
-                        Add to shopping cart
-                      </button>
-                    </div>
-                  )} */}
-              </section>
             </div>
           </div>
         </div>
