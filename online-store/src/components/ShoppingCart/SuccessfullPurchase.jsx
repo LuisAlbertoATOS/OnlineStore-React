2cb998375db36ea8cad75ac278dd260241bdd9dd
Luis Perez <luis-alberto.perez@atos.net>
2022-09-26 12:35:35 -0500

    sales are being created

--- online-store/src/components/ShoppingCart/SuccessfullPurchase.jsx
+++ online-store/src/components/ShoppingCart/SuccessfullPurchase.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
 
-const SuccessfullPurchase = () => {
+const SuccessfullPurchase = ({data}) => {
   return (
     <div className="italic font-semibold text-center text-2xl text-slate-100 bg-green-600 m-5">
       SuccessfullPurchase
+      <p>
+        {data}
+      </p>
     </div>
   );
 };
