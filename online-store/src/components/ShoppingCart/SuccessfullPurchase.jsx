ad61c3486aec6c3326692b37196864712a36e2e9
Luis Perez <luis-alberto.perez@atos.net>
2022-09-28 10:48:20 -0500

    ui fix

--- online-store/src/components/ShoppingCart/SuccessfullPurchase.jsx
+++ online-store/src/components/ShoppingCart/SuccessfullPurchase.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const SuccessfullPurchase = () => {
   return (
     <div className="italic font-semibold text-center text-2xl pt-4 pb-4 rounded text-slate-100 bg-green-600 m-5">
-      SuccessfullPurchase
+      Successfull Purchase
     </div>
   );
 };
