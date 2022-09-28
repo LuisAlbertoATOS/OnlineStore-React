115973f42080ebdaf128e185fbf66e8a2722050c
Luis Perez <luis-alberto.perez@atos.net>
2022-09-28 11:57:04 -0500

    ui details

--- online-store/src/components/ShoppingCart/ShoppingCart.jsx
+++ online-store/src/components/ShoppingCart/ShoppingCart.jsx
@@ -12,16 +12,20 @@ const ShoppingCart = () => {
     shoppingCartContext,
     setShoppingCartContext,
     totalPrice,
-    setTotalPrice
+    setTotalPrice,
   } = useShoppingCartContext();
 
   const deleteItem = (productId) => {
     return () => {
-      if (window.confirm('Are you sure you want to delete this product?')) {
-        const newShoppingCart = shoppingCartContext.filter((items) => items.productId !== productId);
-        const prod = shoppingCartContext.filter((items) => items.productId === productId);
+      if (window.confirm("Are you sure you want to delete this product?")) {
+        const newShoppingCart = shoppingCartContext.filter(
+          (items) => items.productId !== productId
+        );
+        const prod = shoppingCartContext.filter(
+          (items) => items.productId === productId
+        );
         const removedPrice = +prod[0].price * +prod[0].quantity;
-        setTotalPrice((totalPrice) => (totalPrice-removedPrice));
+        setTotalPrice((totalPrice) => totalPrice - removedPrice);
         setShoppingCartContext(newShoppingCart);
       }
     };
@@ -36,107 +40,116 @@ const ShoppingCart = () => {
   };
 
   return (
-    <section className="h-screen w-screen bg-blue-300">
-      <Navbar />
-      <div className="align-content-center container mx-auto mt-10 w-5/6 pl-5">
-        <div className="flex shadow-md my-10">
-          <div className="w-3/4 bg-white px-10 py-10">
-            <div className="flex justify-between border-b pb-8">
-              <h1 className="font-semibold text-2xl">Shopping Cart</h1>
-              <h2 className="font-semibold text-2xl">
-                {shoppingCartContext?.length} Items
-              </h2>
-            </div>
-            <div className="flex mt-10 mb-5">
-              <h3 className="font-semibold text-center text-gray-600 text-xs uppercase w-1/2">
-                Product Details
-              </h3>
-              <h3 className="font-semibold text-center text-gray-600 text-xs uppercase w-1/6">
-                Quantity
-              </h3>
-              <h3 className="font-semibold text-center text-gray-600 text-xs uppercase w-1/6">
-                Price
-              </h3>
-              <h3 className="font-semibold text-center text-gray-600 text-xs uppercase w-1/6">
-                Total
-              </h3>
-            </div>
-
-            {shoppingCartContext?.length === 0 && (
-              <p>No items in the shopping cart</p>
-            )}
-            {shoppingCartContext?.length > 0 &&
-              shoppingCartContext.map((item) => {
-                return (
-                  <div className="border-double border-4 border-blue-500 flex items-center hover:bg-gray-100 -mx-8 pr-6 pl-3 py-5 mb-2">
-                    <div className="flex w-1/2">
-                        <FontAwesomeIcon icon={faTrash} className="px-5 fa-xl self-center hover:text-red-500 cursor-pointer" onClick={deleteItem(item?.productId)}/>
-                      <div className="w-40">
-                        <img className="h-24" src={item.image} alt="" />
-                      </div>
-                      <div className="flex flex-col justify-between self-center flex-grow">
-                        <span className="font-bold text-1xl">{item?.name}</span>
-                      </div>
-                    </div>
-                    <span className="text-center w-1/6 font-semibold text-sm">
-                      {item.quantity}
-                    </span>
-                    <span className="text-center w-1/6 font-semibold text-sm text-green-600">
-                      ${item?.price}
-                    </span>
-                    <span className="text-center w-1/6 font-semibold text-sm text-green-600">
-                      ${item?.price * item.quantity}
-                    </span>
-                  </div>
-                );
-              })}
-
-            <Link
-              to={"/"}
-              className="flex font-semibold text-blue-900 text-sm mt-10"
-            >
-              <svg
-                className="fill-current mr-2 text-blue-900 w-4"
-                viewBox="0 0 448 512"
-              >
-                <path d="M134.059 296H436c6.627 0 12-5.373 12-12v-56c0-6.627-5.373-12-12-12H134.059v-46.059c0-21.382-25.851-32.09-40.971-16.971L7.029 239.029c-9.373 9.373-9.373 24.569 0 33.941l86.059 86.059c15.119 15.119 40.971 4.411 40.971-16.971V296z" />
-              </svg>
-              Continue Shopping
-            </Link>
-          </div>
-
-          <div id="summary" className="bg-blue-50 w-1/4 px-8 py-10">
-            <h1 className="font-semibold text-2xl border-b pb-8">
-              Order Summary
-            </h1>
-            <div className="flex justify-between mt-10 mb-5">
-              <span className="font-semibold text-sm uppercase">Items 3</span>
-              <span className="font-semibold text-sm">${totalPrice}</span>
-            </div>
-            <div>
-              <label className="font-medium inline-block mb-3 text-sm uppercase">
-                Shipping
-              </label>
-              <select className="block p-2 text-gray-600 w-full text-sm">
-                <option>Standard shipping - $10.00</option>
-              </select>
-            </div>
-
-            <div className="border-t mt-8">
-              <div className="flex font-semibold justify-between py-6 text-sm uppercase">
-                <span>Total cost</span>
-                <span>${+totalPrice + 10}</span>
+    <section className="bg-blue-300 h-full">
+      <section className="w-screen bg-blue-300">
+        <Navbar />
+        <div className="align-content-center container mx-auto mt-10 w-5/6 pb-10">
+          <div className="flex shadow-md">
+            <div className="w-3/4 bg-white px-5 py-10">
+              <div className="flex justify-between border-b pb-8">
+                <h1 className="font-semibold text-2xl">Shopping Cart</h1>
+                <h2 className="font-semibold text-2xl">
+                  {shoppingCartContext?.length} Item
+                  {shoppingCartContext?.length > 1 && "s"}
+                </h2>
               </div>
-              <button
-                onClick={doCheckout}
-                className="bg-blue-900 font-semibold hover:bg-indigo-600 py-3 text-sm text-white uppercase w-full"
+              <div className="flex mt-10 mb-5">
+                <h3 className="font-semibold text-center text-gray-600 text-xs uppercase w-1/2">
+                  Product Details
+                </h3>
+                <h3 className="font-semibold text-center text-gray-600 text-xs uppercase w-1/6">
+                  Quantity
+                </h3>
+                <h3 className="font-semibold text-center text-gray-600 text-xs uppercase w-1/6">
+                  Price
+                </h3>
+                <h3 className="font-semibold text-center text-gray-600 text-xs uppercase w-1/6">
+                  Total
+                </h3>
+              </div>
+
+              {shoppingCartContext?.length === 0 && (
+                <p>No items in the shopping cart</p>
+              )}
+              {shoppingCartContext?.length > 0 &&
+                shoppingCartContext.map((item) => {
+                  return (
+                    <div className="border-double border-4 border-blue-500 rounded-lg flex items-center hover:bg-gray-100 mx-1 pr-6 pl-3 py-5 mb-2">
+                      <div className="flex w-1/2">
+                        <FontAwesomeIcon
+                          icon={faTrash}
+                          className="px-5 fa-xl self-center hover:text-red-500 cursor-pointer"
+                          onClick={deleteItem(item?.productId)}
+                        />
+                        <div className="w-40">
+                          <img className="h-24" src={item.image} alt="" />
+                        </div>
+                        <div className="flex flex-col justify-between self-center flex-grow">
+                          <span className="font-bold text-1xl">
+                            {item?.name}
+                          </span>
+                        </div>
+                      </div>
+                      <span className="text-center w-1/6 font-semibold text-sm">
+                        {item.quantity}
+                      </span>
+                      <span className="text-center w-1/6 font-semibold text-sm text-green-600">
+                        ${item?.price}
+                      </span>
+                      <span className="text-center w-1/6 font-semibold text-sm text-green-600">
+                        ${item?.price * item.quantity}
+                      </span>
+                    </div>
+                  );
+                })}
+
+              <Link
+                to={"/"}
+                className="flex font-semibold text-blue-900 text-sm mt-10"
               >
-                Checkout
-              </button>
+                <svg
+                  className="fill-current mr-2 text-blue-900 w-4"
+                  viewBox="0 0 448 512"
+                >
+                  <path d="M134.059 296H436c6.627 0 12-5.373 12-12v-56c0-6.627-5.373-12-12-12H134.059v-46.059c0-21.382-25.851-32.09-40.971-16.971L7.029 239.029c-9.373 9.373-9.373 24.569 0 33.941l86.059 86.059c15.119 15.119 40.971 4.411 40.971-16.971V296z" />
+                </svg>
+                Continue Shopping
+              </Link>
+            </div>
+
+            <div id="summary" className="bg-blue-50 w-1/4 px-8 py-10">
+              <h1 className="font-semibold text-2xl border-b pb-8">
+                Order Summary
+              </h1>
+              <div className="flex justify-between mt-10 mb-5">
+                <span className="font-semibold text-sm uppercase">Items</span>
+                <span className="font-semibold text-sm">${totalPrice}</span>
+              </div>
+              <div>
+                <label className="font-medium inline-block mb-3 text-sm uppercase">
+                  Shipping
+                </label>
+                <select className="block p-2 text-gray-600 w-full text-sm">
+                  <option>Standard shipping - $10.00</option>
+                </select>
+              </div>
+
+              <div className="border-t mt-8">
+                <div className="flex font-semibold justify-between py-6 text-sm uppercase">
+                  <span>Total cost</span>
+                  <span>${+totalPrice + 10}</span>
+                </div>
+                <button
+                  onClick={doCheckout}
+                  className="bg-blue-900 font-semibold hover:bg-indigo-600 py-3 text-sm text-white uppercase w-full"
+                >
+                  Checkout
+                </button>
+              </div>
             </div>
           </div>
         </div>
-      </div>
+      </section>
     </section>
   );
 };
