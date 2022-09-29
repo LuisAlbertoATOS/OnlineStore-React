cfd3d9291a76078b581b9e4826f65ecc082c944b
AlanGarciaAtos <alan.garcia@atos.net>
2022-09-29 10:57:53 -0500

    ShoppingCart folder cleanup, div to section, semantic html

--- online-store/src/components/ShoppingCart/SuccessfullPurchase.jsx
+++ online-store/src/components/ShoppingCart/SuccessfullPurchase.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 
 const SuccessfullPurchase = () => {
   return (
-    <div className="italic font-semibold text-center text-2xl pt-4 pb-4 rounded text-slate-100 bg-green-600 m-5">
+    <section className="italic font-semibold text-center text-2xl pt-4 pb-4 rounded text-slate-100 bg-green-600 m-5">
       Successfull Purchase
-    </div>
+    </section>
   );
 };
 
