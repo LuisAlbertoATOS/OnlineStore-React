004349b55137519da0b81bb49e912ef5c2b35b85
AlanGarciaAtos <alan.garcia@atos.net>
2022-09-30 14:41:06 -0500

    Add some testing and 404 page and herp

--- online-store/src/components/NotFound.jsx
+++ online-store/src/components/NotFound.jsx
@@ -3,17 +3,26 @@ import { Link } from 'react-router-dom';
 
 const NotFound = () => {
   return (
-    <section>
-      It doesn't exist the link. Please return to home!
-      <Link to={`/`}>
-        <button
-          type="button"
-          className="inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out disabled:bg-blue-400"
-        >
-          Home
+    <>
+      <section class="h-screen w-full flex flex-col justify-center items-center bg-blue-800">
+        <h1 class="text-9xl font-extrabold text-white tracking-widest">404</h1>
+        <div class="bg-yellow-300 px-2 text-sm rounded rotate-12 absolute">
+          Page Not Found
+        </div>
+        <button class="mt-5">
+          <Link
+            to={'/'}
+            class="relative inline-block text-sm font-medium text-yellow-300 group active:text-yellow-500 focus:outline-none focus:ring"
+          >
+            <span class="absolute inset-0 transition-transform translate-x-0.5 translate-y-0.5 bg-yellow-300 group-hover:translate-y-0 group-hover:translate-x-0"></span>
+
+            <span class="relative block px-8 py-3 bg-blue-800 border border-current">
+              Return to Home
+            </span>
+          </Link>
         </button>
-      </Link>
-    </section>
+      </section>
+    </>
   );
 };
 
