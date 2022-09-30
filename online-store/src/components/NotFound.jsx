06ae42580aea3dde5dfe5356435a6da34de3483e
AlanGarciaAtos <alan.garcia@atos.net>
2022-09-30 14:43:10 -0500

    Ttsy

--- online-store/src/components/NotFound.jsx
+++ online-store/src/components/NotFound.jsx
@@ -4,6 +4,7 @@ import { Link } from 'react-router-dom';
 const NotFound = () => {
   return (
     <>
+      {/* Hola */}
       <section class="h-screen w-full flex flex-col justify-center items-center bg-blue-800">
         <h1 class="text-9xl font-extrabold text-white tracking-widest">404</h1>
         <div class="bg-yellow-300 px-2 text-sm rounded rotate-12 absolute">
