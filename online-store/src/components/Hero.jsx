98607a708a23079b2587e6223c682db1c022c541
AlanGarciaAtos <alan.garcia@atos.net>
2022-09-30 15:11:01 -0500

    Fix warnings

--- online-store/src/components/Hero.jsx
+++ online-store/src/components/Hero.jsx
@@ -2,22 +2,22 @@ import React from 'react';
 
 const Hero = () => {
   return (
-    <section class="relative overflow-hidden ">
-      <div class="container flex flex-col-reverse lg:flex-row items-center gap-12 mt-14 mx-auto lg:mt-28">
+    <section className="relative overflow-hidden ">
+      <div className="container flex flex-col-reverse lg:flex-row items-center gap-12 mt-14 mx-auto lg:mt-28">
         {/* <!-- Content --> */}
-        <div class="flex flex-1 flex-col items-center lg:items-start">
-          <h2 class="text-bookmark-blue font-bold text-3xl md:text-4 lg:text-5xl text-center lg:text-left mb-6">
+        <div className="flex flex-1 flex-col items-center lg:items-start">
+          <h2 className="text-bookmark-blue font-bold text-3xl md:text-4 lg:text-5xl text-center lg:text-left mb-6">
             Check the new headphones!
           </h2>
-          <p class="text-bookmark-grey text-lg text-center lg:text-left mb-6">
+          <p className="text-bookmark-grey text-lg text-center lg:text-left mb-6">
             HyperX Cloud Stinger is the ideal headset for gamers looking for
             lightweight comfort, superior sound quality and added convenience.
           </p>
         </div>
         {/* <!-- Image --> */}
-        <div class="flex justify-center flex-1 mb-10 md:mb-16 lg:mb-0 z-10">
+        <div className="flex justify-center flex-1 mb-10 md:mb-16 lg:mb-0 z-10">
           <img
-            class="w-5/6 h-5/6 sm:w-3/4"
+            className="w-5/6 h-5/6 sm:w-3/4"
             src="https://i.postimg.cc/KcBqRbtp/unknown-removebg-preview.png"
             alt=""
           />
@@ -25,8 +25,9 @@ const Hero = () => {
       </div>
       {/* <!-- Rounded Rectangle --> */}
       <div
-        class="
+        className="
           hidden
+          sm:block
           md:block
           overflow-hidden
           bg-blue-800
