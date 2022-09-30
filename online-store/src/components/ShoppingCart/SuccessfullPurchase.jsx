3c3c38042a392fa4ab1b7f8c29aa7e605ff8ad24
AlanGarciaAtos <alan.garcia@atos.net>
2022-09-30 14:42:05 -0500

    Delete some elkements

--- online-store/src/components/ShoppingCart/SuccessfullPurchase.jsx
+++ online-store/src/components/ShoppingCart/SuccessfullPurchase.jsx
@@ -1,19 +1,9 @@
 import React from 'react';
 
 const SuccessfullPurchase = () => {
-  let a = 3;
-  let b = 3;
-
   return (
     <section className="italic font-semibold text-center text-2xl pt-4 pb-4 rounded text-slate-100 bg-green-600 m-5">
       Successfull Purchase
-      <ul>
-        <li>Apple</li>
-        <li>Banana</li>
-        <li>Orange</li>
-      </ul>
-      <h1 data-testid="mytestid">Hello</h1>
-      <span title="sum">{a + b}</span>
     </section>
   );
 };
